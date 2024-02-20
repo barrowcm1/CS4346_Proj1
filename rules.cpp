1d9345d73b0672160fc8c0fcf215b108842c5d7e
Caitlyn Barrow <cmb426@txstate.edu>
2024-02-20 00:05:18 -0600

    skeleton

--- rules.cpp
+++ rules.cpp
@@ -1,1 +1,33 @@
+#include "rules.h"
+
+using namespace std;
+
+// Symptoms list for each attack type
+const vector<AttackSymptoms> attackSymptoms = {
+    {AttackType::PHISHING, {
+        "Reports of users falling victim to deceptive messages or emails",
+        "Users have reported receiving suspicious emails requesting sensitive info",
+        "User has provided sensitive information after clicking on a suspicious link",
+        "User has encountered unexpected redirects to login pages/forms asking for credentials"
+    }}
+    // so on.... so forth
+
+};
+
+/* function to identify the attack by comparing the vector created in either
+    forward or backwardchaining.cpp to one of the vectors above
+    ex.) AttackType identifyAttack( ....) {
+        .........
+    }
+        return attackType; */
+
+// Recommend preventative measures
+void recommendedPreventions(AttackType attackType) {
+    switch (attackType) {
+        case AttackType::PHISHING:
+        cout << "blah blah blah";
+        break;
+        //and so on......
+    }
+}
 
--- rules.h
+++ rules.h
@@ -1,8 +1,9 @@
 #ifdef RULES_H
 #define RULES_H
-
 #include <string>
 #include <vector>
+
+using namespace std;
 
 // Define different attack types
 class AttackType {
@@ -13,5 +14,17 @@ class AttackType {
     MAN_IN_THE_MIDDLE,
     BRUTE_FORCE
 };
+
+// Define symptoms; combines attack type and symptoms into one entity
+struct AttackSymptoms {
+    AttackType type;
+    vector<string> symptoms;
+};
+
+/* !!!!! Still need to include a function to identify attack based on symptoms
+ex.) AttackType identifyAttack(.......); etc... */
+
+// Function to reccomend possible preventions
+void recommendedPreventions(AttackType attackType);
 
 #endif
