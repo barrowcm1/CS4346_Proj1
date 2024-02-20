9d502233feb73b328d912f71e86ac761e3fd70ad
Caitlyn Barrow <cmb426@txstate.edu>
2024-02-19 20:09:38 -0600

    Put definitions in wrong file

--- backward_chaining.cpp
+++ backward_chaining.cpp
@@ -1,11 +1,1 @@
-#include <string>
-#include <vector>
 
-class AttackType {
-    PHISHING,
-    MALWARE,
-    DDOS,
-    CYBER_ATTACK,
-    MAN_IN_THE_MIDDLE,
-    BRUTE_FORCE
-};
--- rules.h
+++ rules.h
@@ -1,1 +1,11 @@
+#include <string>
+#include <vector>
 
+class AttackType {
+    PHISHING,
+    MALWARE,
+    DDOS,
+    CYBER_ATTACK,
+    MAN_IN_THE_MIDDLE,
+    BRUTE_FORCE
+};
