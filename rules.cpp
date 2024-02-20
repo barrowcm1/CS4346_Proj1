e72ded11f1eaf7cc9ff304d3b04f7b45d493cb79
Caitlyn Barrow <cmb426@txstate.edu>
2024-02-20 01:15:48 -0600

    rules.cpp edit for recc preventions

--- rules.cpp
+++ rules.cpp
@@ -1,4 +1,7 @@
 #include "rules.h"
+#include <vector>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -25,9 +28,36 @@ const vector<AttackSymptoms> attackSymptoms = {
 void recommendedPreventions(AttackType attackType) {
     switch (attackType) {
         case AttackType::PHISHING:
-        cout << "blah blah blah";
+        cout << "It is recommended that you block popups and ads, change any passwords you may have \n";
+        cout << "given to the malicious sender, and report any further attempts at phishing to abuse@txstate.edu \n";
         break;
-        //and so on......
+
+        case AttackType::MALWARE:
+        cout << "It is recommended that you scan and clean your computer with an antivirus software \n";
+        cout << "or seek help from a Computer Technician. \n";
+        break;
+
+        case AttackType::DDOS:
+        cout <<"It is recommended that you patch youre server or implement network monitoring and \n";
+        cout << "filtering with an authentication firewall. \n";
+        break;
+
+        case AttackType::CYBER_ATTACK:
+        cout << "It is recommended that you contain the cyber breach by disabling remote access and \n";
+        cout << "implementing better authentication features. \n";
+        break;
+
+        case AttackType::MAN_IN_THE_MIDDLE:
+        cout << "It is recommended that you get a VPN and set up a strong WEP/WAP encryption. \n";
+        break;
+
+        case AttackType::BRUTE_FORCE:
+        cout << "It is recommended that you change all your passwords and have protective measures \n";
+        cout << "implemented that will lock your account after a certain number of failed login attempts. \n";
+        break;
+
+        default:
+        cout <<"No preventive solutions are recommended. \n";
     }
 }
 
