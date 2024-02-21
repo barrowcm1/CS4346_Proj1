14f69bc7d7458fe5cdd446d2142290579ccbceb6
Caitlyn Barrow <cmb426@txstate.edu>
2024-02-20 18:32:26 -0600

    backwardchain comments

--- backward_chaining.cpp
+++ backward_chaining.cpp
@@ -1,1 +1,79 @@
+/*
+        
+REWORDED VARIABLES:
 
+        "Computer Slow", "E-mail From Suspicious Address", "Credential Request", "Urgent Action Demands",
+        "Compromised Email", "Internet Redirects/Pop-ups", "Modification of Network Data", "Unable to Login", "Suspicious Traffic", "Flood of Traffic", 
+        "Inability to Retrieve Sensor Data", "Computer Won't Shutdown", "Repeated Error Messages", "Computer Freezing", "Data Manipulation", "System Vulenrability",
+        "Suspicious E-mail Links", "Malicious Software Installation", "Information Leakage", "Inappropriate ADS", "Demanding Ransom"};
+
+RENUMBERED VARIABLES:
+        vector<int> cyber_clause = {0,1,4,7,10,11,18,19,20};
+        vector<int> malware_clause = {0,7,12,13,14,15,16};
+        vector<int> phishing_clause = {0,1,2,3,4,5,16,17,20};
+        vector<int> ddos_clause = {0,6,7,8,9,10};
+        vector<int> man_in_the_middle_clause = {0,4,5,6,7,8,18};
+        vector<int> brute_force_clause = {0,1,2,4,5,8,18,19};
+
+cout << "Are you experiencing any lags or delays in your computer preformance? (yes/no)";
+cin >> answer;
+
+cout << "Have you recieved any suspicious E-mail requests from an unknown address? (yes/no) \n";
+cin >> answer;
+
+cout << "Have you recieved any unsolicited E-mails requesting private information or credentials? (yes/no) \n";
+cin >> answer;
+
+cout << "Have you received any messages via E-mail or browser pop-up that request some sort of 'Urgent Action'? (yes/no) \n";
+cin >> answer;
+
+cout << "Have you recieved a notification that your E-mail has been compromised? (yes/no) \n";
+cin >> answer;
+
+cout << "Are you experiencing an overwhleming amount of internet pop-ups or redirects? (yes/no) \n";
+cin >> answer;
+
+cout << " Have you noticed any signs of unauthorized intereception or modification of network data? (yes/no) \n";
+cin >> answer; 
+
+cout << "Are your usual login credentials not working or coming back as invalid? (yes/no) \n"
+cin >> answer;
+
+cout << "Is there traffic originatin from multiple, unrelated sources? (yes/no) \n";
+cin >> answer;
+
+cout << "Is there a sudden increase in network traffic volume? (yes/no) \n";
+cin >> answer;
+
+cout << "Are you having trouble retreiving sensor data that may be a result of data corruption? (yes/no) \n";
+cin >> answer;
+
+cout << "Are you experiencing trouble shutting down or restarting your computer? (yes/no) \n";
+cin >> answer;
+
+cout << "Is your computer frequently freezing or becoming unresponsive? (yes/no) \n";
+cin >> answer;
+
+cout << "Is there any evidence of data manipulation, tampering, or destruction? (yes/no) \n";
+cin >> answer;
+
+cout << "Are there any indications of system vulnerabilities, exploitation, or malware propagation? (yes/no) \no";
+cin >> answer;
+
+cout << "Have you received any E-mails containing any suspicious links? (yes/no)";
+cin >> answer;
+
+cout << "Is there evidence of malicious software installation or unusual system behavior? (yes/no)";
+cin >> answer;
+
+cout << "Is there evidence of unauthorized data exfiltration or information leakage? (yes/no)";
+cin >> answer;
+
+cout << "Have you noticed any innapropriate ADs or popups on your computer? (yes/no)";
+cin >> answer;
+
+cout << "Are there signs of ransome demands, extortion attempts, or finanicial fraud? (yes/no)";
+cin >> answer;
+
+
+*/
